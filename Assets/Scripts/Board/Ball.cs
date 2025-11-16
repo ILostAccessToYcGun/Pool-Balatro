39c050eebfd14e850583100a5afdd996f2e465d9
Yoga Gunawan <ycgun32@gmail.com>
2025-11-15 21:03:18 -0500

    H O L E

--- Assets/Scripts/Board/Ball.cs
+++ Assets/Scripts/Board/Ball.cs
@@ -23,7 +23,7 @@ public class Ball : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Debug.Log(rb.linearVelocity.magnitude);
+        //Debug.Log(rb.linearVelocity.magnitude);
         rb.linearVelocity *= (1 - (weight * Time.deltaTime));
         previousVel = rb.linearVelocity;
     }
@@ -31,31 +31,17 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //if this hits a wall, bounce off it with a bounciness var
-        //w = v – (2 * (v . n) * n)
+        //Incorporate the score manager here cuz bounces give blue score 
+
         Vector3 normal;
         Vector2 bounceVel;
-
-        //ContactPoint2D[] contactPoint2Ds = collision.contacts;
-        //foreach (ContactPoint2D contacts in contactPoint2Ds)
-        //{
-        //    if (contacts.rigidbody.gameObject.CompareTag("Wall"))
-        //    {
-        //        normal = contacts.normal;
-        //    }
-        //}
 
         normal = collision.GetContact(0).normal;
 
         float dot = Vector2.Dot(previousVel, normal);
 
         bounceVel = previousVel - (2.0f * dot * (Vector2)normal);
-        bounceVel.Normalize();
-        bounceVel = bounceVel * previousVel.magnitude * bounciness;
-
-        //i need to incorporate weight into here
-
-        //also there is a ratio based on the dot product of how much energy is given
+        bounceVel = bounceVel.normalized * previousVel.magnitude * bounciness;
 
         //if we hit a ball, give half of the speed to the ball
         Ball tryGet = collision.gameObject.GetComponent<Ball>();
@@ -67,11 +53,5 @@ public class Ball : MonoBehaviour
         }
         rb.linearVelocity += bounceVel;
 
-
-        Debug.Log(rb.linearVelocity);
-        Debug.Log(normal);
-        Debug.Log(dot);
-        //get the surface normal of the wall
-        //Vector3 normal = collision.GetContact(0).normal;
     }
 }
---
+++ Assets/Scripts/Board/Hole.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    [SerializeField] Collider2D col;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //also maybe check if the velocity is not zoom cuz it would skip over the hole
+        //but for now nah
+
+        //if the center of a ball is over a hole
+        Ball tryGet = collision.gameObject.GetComponent<Ball>();
+        if (tryGet != null)
+        {
+            //velocity checking
+            //Rigidbody2D rb2 = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (col.bounds.Contains(tryGet.gameObject.transform.position)) //not sure if this is the right collider
+            {
+                Destroy(tryGet.gameObject);
+                Debug.Log("Ball is sunk");
+
+                //Incorporate the score manager here cuz sinking a ball gives red score
+            }
+
+        }
+    }
+}
